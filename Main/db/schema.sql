b005d7dfd6e04986db0e14f3b4e420662a7d42bb
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-03 12:00:05 -0700

    set up the basic outline of what will be needed for schema and seeds

---
+++ Main/db/schema.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+--need to create tables for departments, roles, and employees
---
+++ Main/db/seeds.sql
@@ -0,0 +1,3 @@
+USE employees;
+
+--need to have values for departments, roles, and employees
