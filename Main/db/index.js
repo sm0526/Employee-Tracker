8e12c2597d8b912bc29183785c5c6e18ad462088
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-03 17:36:43 -0700

    added in 2 functions to index.js in the db folder, one to find all managers the other to find all employees by manager

--- Main/db/index.js
+++ Main/db/index.js
@@ -11,6 +11,20 @@ class DB {
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
     }
+
+    findAllPossibleManagers(employeeId) {
+        return this.connection.promise().query(
+            "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+            employeeId
+        );
+    }
+
+    findAllEmployeesByManager(managerId) {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+            managerId
+        );
+    }
 }
 
 module.exports = new DB(connection);
