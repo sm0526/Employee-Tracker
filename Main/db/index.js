1a13729f376632d4354728e72c8e0741f7eccc8a
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-03 17:21:19 -0700

    added in the function to find all the employees to index.js in db folder, also added export at the bottom of this file to prevent forgetting it

--- Main/db/index.js
+++ Main/db/index.js
@@ -6,4 +6,11 @@ class DB {
         this.connection = connection;
     }
     //need to create functions that go with the db ones created on the functions in index.js
+    findAllEmployees() {
+        return this.connection.promise().query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+    }
 }
+
+module.exports = new DB(connection);
