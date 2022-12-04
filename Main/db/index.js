8f21f0d10a99bd8e960c28981d11e3dc2df6405a
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-03 17:05:58 -0700

    created index.js inside db folder, basic outline require the connect.js also in db folder, set up class reference of the connection and comments as a reminder of I need to complete in this file

---
+++ Main/db/index.js
@@ -0,0 +1,9 @@
+const connection = require("./connect");
+
+class DB {
+    //reference to the connection just in case
+    constructor(connection) {
+        this.connection = connection;
+    }
+    //need to create functions that go with the db ones created on the functions in index.js
+}
